9da97bdfd03c92b777033af22c44a195c1b8c4da
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-02-11 11:37:19 -0800

    Create guesser.cpp

---
+++ guesser.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> guesses(unsigned known) {
+  std::vector<std::string> response {
+   "000000",
+   "badbad",
+  };
+  
+  return response;
+}
